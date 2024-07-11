f634e38df2f2a8c95424f0df8a44ccae24aaa081
mohamed <mohamedshalaby2@aol.com>
2024-07-11 07:59:55 +0000

    rer

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -11,4 +11,3 @@ print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
-carrie@ubuntu:~/0x02-minoperations$
